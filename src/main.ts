120d2e850b8c6586a1c6bb7edd8d5d3458d3f0fe
Sadaf noor <noorsadaf183@gmail.com>
2024-11-06 17:00:08 +0530

    merged

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppLogger } from './services/loggers/winston.config';
 import { activeBotQueue } from 'config/constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Use AppLogger as the global logger
-  const appLogger = app.get(AppLogger);
-  app.useLogger(appLogger);
-
-  appLogger.log('Starting Nest application with AppLogger');
-  
   await app.listen(3000);
-  appLogger.log('Nest application successfully started');
-  
-  appLogger.log(`------------------------------Exchange set to: ⚡${process.env.exchange}------------------------------`);
-  appLogger.log(`---------------------- Bot Type : ${process.env.botType} -------------------------------`);
-  appLogger.log(`------------📬 Queue : ${activeBotQueue} ----------`);
+  console.log(`------------------------------Exchange set to: ⚡${process.env.exchange}------------------------------`);
+  console.log(`---------------------- Bot Type : ${process.env.botType} -------------------------------`)
+console.log(`------------📬 Queue : ${activeBotQueue} ----------`)
 }
-
 bootstrap();
