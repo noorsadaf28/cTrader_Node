09e1efae50662c333c731c3ccb5119ecd9eca155
Saurabh Pandey <saurabhp643@gmail.com>
2024-12-02 18:08:06 +0530

    Won event updates with phases

--- src/services/botProcess/evaluationBot.process.ts
+++ src/services/botProcess/evaluationBot.process.ts
@@ -2,45 +2,45 @@ import { Job } from "bull";
 import { PhaseSettings } from "src/data/rulesData";
 import { BaseBotProcess } from "src/services/baseBotprocess.service";
 import * as dayjs from 'dayjs';
-export class EvaluationBotProcess extends BaseBotProcess{
-    
-    async startChallenge(botInfo:Job) {
+export class EvaluationBotProcess extends BaseBotProcess {
+
+    async startChallenge(botInfo: Job) {
         console.log("bot detail--------", botInfo.data);
         const phaseConnection = this.connectPhase(botInfo);
-        if(phaseConnection){
+        if (phaseConnection) {
             this.sendOnInit(botInfo);
             this.runOrderPolling(botInfo)
         }
-        
+
     }
-    async connectPhase(botInfo:Job) {
-        try{
+    async connectPhase(botInfo: Job) {
+        try {
             const phaseSettings = PhaseSettings[botInfo.data.Phase];
-            if(phaseSettings){
+            if (phaseSettings) {
                 const botData = botInfo.data;
                 const connectedPhaseData = Object.assign({}, botData, phaseSettings);
-                connectedPhaseData.max_daily_currency = parseInt(connectedPhaseData.Initial_balance)*(connectedPhaseData.max_daily_loss/100)
-                connectedPhaseData.max_total_currency = parseInt(connectedPhaseData.Initial_balance)*(connectedPhaseData.max_loss/100)
-                connectedPhaseData.profitCurrency = parseInt(connectedPhaseData.Initial_balance)*(connectedPhaseData.profit_target/100)
+                connectedPhaseData.max_daily_currency = parseInt(connectedPhaseData.Initial_balance) * (connectedPhaseData.max_daily_loss / 100)
+                connectedPhaseData.max_total_currency = parseInt(connectedPhaseData.Initial_balance) * (connectedPhaseData.max_loss / 100)
+                connectedPhaseData.profitCurrency = parseInt(connectedPhaseData.Initial_balance) * (connectedPhaseData.profit_target / 100)
                 connectedPhaseData.challenge_begins = Date
                 const tempInfo = connectedPhaseData;
                 botInfo.update(tempInfo)
                 console.log("🚀 ~ EvaluationBotProcess ~ connectPhase ~ botInfo:", botInfo.data)
                 return true;
             }
-            else{
+            else {
                 return false;
             }
         }
-        catch(error){
+        catch (error) {
             console.log("🚀 ~ EvaluationBotProcess ~ connectPhase ~ error:", error)
         }
     }
-    async handleBot(botInfo:Job){
+    async handleBot(botInfo: Job) {
         this.stopBot(botInfo);
     }
-    async sendOnInit(botInfo:Job){
-        try{
+    async sendOnInit(botInfo: Job) {
+        try {
 
             botInfo.data.status = "Active";
             botInfo.data.request_type = "OnInit";
@@ -48,81 +48,137 @@ export class EvaluationBotProcess extends BaseBotProcess{
             botInfo.data.challenge_begins = (dayjs(Date.now()).format('YYYY-MM-DD')).toString();
             botInfo.data.daily_kod = "false";
             botInfo.data.total_kod = "false";
-            botInfo.data.challenge_won= "false";
-            botInfo.data.challenge_ends= "";
+            botInfo.data.challenge_won = "false";
+            botInfo.data.challenge_ends = "";
             const tempInfo = botInfo.data;
             botInfo.update(tempInfo);
             console.log("🚀 ~ EvaluationBotProcess ~ sendOnInit ~ botInfo:", botInfo.data)
 
             this.IEvaluationInterface.rulesEvaluation(botInfo)
         }
-        catch(error){
+        catch (error) {
             console.log("🚀 ~ EvaluationBotProcess ~ sendOnInit ~ error:", error)
-            
+
         }
     }
-    async runOrderPolling(botInfo:Job){
+    async runOrderPolling(botInfo: Job) {
         //console.log("🚀 ~ EvaluationBotProcess ~ runOrderPolling ~ botInfo:", botInfo)
-        try{
-            while(await botInfo.data.running){
-                const {interval} = await botInfo.data;
+        try {
+            while (await botInfo.data.running) {
+                const { interval } = await botInfo.data;
                 this.IOrderInterface.pollPositions(botInfo);
                 this.IEvaluationInterface.subscribeToSpotQuotes(botInfo);
                 await new Promise(resolve => setTimeout(resolve, interval * 1000));
             }
         }
-        catch(error){
+        catch (error) {
             console.log("🚀 ~ EvaluationBotProcess ~ runOrderPolling ~ error:", error)
-            
+
         }
     }
-    async sendWon(botInfo:Job){
-        try{
+    async sendWon(botInfo: Job) {
+        try {
 
             botInfo.data.status = "Won";
             botInfo.data.request_type = "Won";
             botInfo.data.accountId = botInfo.data.traderLogin;
-            botInfo.data.challenge_won = "false";
-            const tempInfo = botInfo.data;
-            botInfo.update(tempInfo);
-            console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ botInfo:", botInfo.data)
+            // const tempInfo = botInfo.data;
+            // botInfo.update(tempInfo);
+            // console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ botInfo:", botInfo.data)
 
-            this.IEvaluationInterface.rulesEvaluation(botInfo)
-            if(botInfo.data.phase === process.env.Phase_1){
-                this.switchToPhase2(botInfo);
+
+            botInfo.data.challenge_won = "true";
+            botInfo.data.challenge_ends = dayjs(Date.now()).format('YYYY-MM-DD');
+            botInfo.data.daily_kod = "false";
+            botInfo.data.total_kod = "false";
+
+            let nextPhase: string | undefined;
+            switch (botInfo.data.phase) {
+                case process.env.Phase_0:
+                    nextPhase = process.env.Phase_1;
+                    break;
+                case process.env.Phase_1:
+                    nextPhase = process.env.Phase_2;
+                    break;
+                case process.env.Phase_2:
+                    nextPhase = "FUNDED";
+                    break;
+                default:
+                    console.log(`User has completed all phases. Current phase: ${botInfo.data.phase}`);
+                    await this.IEvaluationInterface.rulesEvaluation(botInfo);
+                    await this.stopChallenge(botInfo);
+                    return;
             }
-        }
-        catch(error){
-            console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ error:", error)
-            
+
+            if (nextPhase !== "FUNDED") {
+                console.log(`Switching from ${botInfo.data.phase} to ${nextPhase}`);
+                botInfo.data.phase = nextPhase;
+                await this.switchToPhase2(botInfo);
+            } else {
+                console.log(`User has reached the FUNDED phase`);
+                botInfo.data.phase = "FUNDED";
+                await this.startChallenge(botInfo); // Restart challenge for funded phase
+                await this.stopChallenge(botInfo); // Finalize
+            }
+        } catch (error) {
+            console.log("🚀 ~ BaseBotProcess ~ sendWon ~ error:", error);
         }
     }
-    async sendTotalKOD(botInfo:Job){
+    async stopChallenge(botInfo: Job) {
+        try {
+            botInfo.data.running = false;
+            const temp = botInfo.data;
+            botInfo.data.update(temp);
+            console.log(" :⛔️:️ Bot Stopped")
+            await this.IBotInterface.stopBot(botInfo)
+        }
+        catch (error) {
+            console.log("🚀 ~ BaseEvaluationService ~ stopChallenge ~ error:", error)
+        }
+    }
+    async sendDailyKOD(botInfo:Job){
         try{
+          botInfo.data.request_type = "DailyKOD";
+          botInfo.data.status = "Failed";
+          botInfo.data.challenge_won = "false";
+          botInfo.data.challenge_ends = dayjs(Date.now()).format('YYYY-MM-DD');
+          botInfo.data.daily_kod = "true",
+          botInfo.data.total_kod = "false"
+          await this.IEvaluationInterface.rulesEvaluation(botInfo);
+          await this.stopChallenge(botInfo)
+        }
+        catch(error){
+          console.log("🚀 ~ BaseEvaluationService ~ sendDailyKOD ~ error:", error)
+        }
+      }
+
+    async sendTotalKOD(botInfo: Job) {
+        try {
 
             botInfo.data.status = "Failed";
             botInfo.data.request_type = "TotalKOD";
             botInfo.data.daily_kod = "false";
             botInfo.data.accountId = botInfo.data.traderLogin;
-            const tempInfo = botInfo.data;
-            botInfo.update(tempInfo);
-            console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ botInfo:", botInfo.data)
+            // const tempInfo = botInfo.data;
+            // botInfo.update(tempInfo);
+            // console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ botInfo:", botInfo.data)
+            await this.stopChallenge(botInfo); // Finalize
 
             this.IEvaluationInterface.rulesEvaluation(botInfo)
         }
-        catch(error){
+        catch (error) {
             console.log("🚀 ~ EvaluationBotProcess ~ sendWon ~ error:", error)
-            
+
         }
     }
-    async switchToPhase2(botInfo:Job){
-        try{
+    async switchToPhase2(botInfo: Job) {
+        try {
             botInfo.data.phase = process.env.Phase_2;
             const tempData = botInfo.data;
             botInfo.update(tempData);
             this.startChallenge(botInfo)
         }
-        catch(error){
+        catch (error) {
             console.log("🚀 ~ EvaluationBotProcess ~ switchToPhase2 ~ error:", error)
         }
     }
