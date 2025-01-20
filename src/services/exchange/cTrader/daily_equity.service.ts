0b623de25e46b43fdbb29585433a9c0ff376e524
Saurabh Pandey <saurabhp643@gmail.com>
2025-01-20 15:45:22 +0530

    trading days

--- src/services/exchange/cTrader/daily_equity.service.ts
+++ src/services/exchange/cTrader/daily_equity.service.ts
@@ -97,7 +97,7 @@ export class DailyEquityService {
         const response: AxiosResponse = await this.httpService
             .get(`${this.spotwareApiUrl}/v2/webserv/traders/`, {
                 params: {
-                    from: dayjs('2025-01-06').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS'),
+                    from: dayjs('2025-01-18').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS'),
                     to: toDate,
                     fields: 'login,balance,minEquityDaily,maxEquityDaily,registrationTimestamp',
                     token: this.apiToken,
@@ -124,10 +124,11 @@ export class DailyEquityService {
                   gmt_date: dayjs().tz('UTC').format('YYYY.MM.DD HH:mm:ss'), // Correct format
                   created_at: dayjs().toISOString(),
                   status: 'pending',
-                  trading_days: openPositionData.tradingDays || 0,
-                  challenge_begins:dayjs(trader.registrationTimestamp).format('YYYY.MM.DD'), // Convert registration timestamp
+                  trading_days: dayjs().diff(dayjs(trader.registrationTimestamp), 'day'), // Calculate trading days
+                  challenge_begins: dayjs(trader.registrationTimestamp).format('YYYY.MM.DD'), // Convert registration timestamp
                   new_status: 'sent',
                 };
+                
             })
         );
 
@@ -205,7 +206,7 @@ export class DailyEquityService {
 
   // Update the equity for traders daily
   async updateDailyEquityForTraders() {
-  const fromDate = dayjs('2025-01-06').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS');
+  const fromDate = dayjs('2025-01-18').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS');
     const toDate = dayjs().startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS');
 
     this.logger.log(`Starting daily equity update for traders from ${fromDate} to ${toDate}`);
